fa16454cbdf8fb9ef3140d69649af2e75c6e3879
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2023-08-27 12:48:12 -0500

    refresh page for click count

--- static/script.js
+++ static/script.js
@@ -12,4 +12,30 @@ document.addEventListener("DOMContentLoaded", function() {
             location.reload();
         });
     });
+
+    const visitButton = document.querySelectorAll('.visitButton');
+    
+    // Add a click event listener to each button
+    visitButton.forEach(function(button) {
+      button.addEventListener('click', function(event) {
+        // Prevent the default action
+        event.preventDefault();
+        // Open the link in a new tab
+        window.open(event.target.href, '_blank');
+        // Refresh the page
+        location.reload();
+      });
+    });
+    // Copy to Clipboard
+    document.querySelectorAll('.copyButton').forEach(function(button) {
+        button.addEventListener('click', function(e) {
+            const url = e.target.getAttribute('data-url');
+            navigator.clipboard.writeText("http://127.0.0.1:5000/"+url).then(function() {
+                e.target.textContent = 'Copied!';
+                setTimeout(function() {
+                    e.target.textContent = 'Copy';
+                }, 2000);
+            });
+        });
+    });
 });

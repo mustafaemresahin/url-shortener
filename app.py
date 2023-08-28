ac194476a58becef677bff230e182fa8918992a6
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2023-08-27 22:22:21 -0500

    add comments to app.py

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
+# Importing required modules from Flask and other Python libraries
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from urllib.parse import urlparse
 import string
 import random
 from datetime import datetime, timedelta
 
+# Initialize Flask app and set secret key for session management
 app = Flask(__name__)
 app.secret_key = 'l&Z1ly3aa!'
 
-# Dictionary to store short_urls
+# Initialize dictionary to store short URLs
 url_list = {}
 
+# Function to generate random short URL
 def generate_short_url():
     characters = string.ascii_letters + string.digits
     short_url = ''.join(random.choice(characters) for i in range(6))
     return short_url
 
+# Function to validate the input URL
 def is_valid_url(url):
     parsed_url = urlparse(url)
     return all([parsed_url.scheme in ['http', 'https'], parsed_url.netloc])
 
+# Index Route
 @app.route('/', methods=['GET', 'POST'])
 def index():
     short_url = None
@@ -26,15 +31,18 @@ def index():
     expiration_time = None
     click_count = 0
 
+    # Handling POST requests
     if request.method == 'POST':
         original_url = request.form['url']
         expiration_time = request.form.get('expiration_time', None)
         current_time = datetime.now()
 
+        # Validate the URL format
         if not is_valid_url(original_url):
             flash('Invalid URL format! Please enter a URL like https://mustafaemresahin.com')
             return render_template('index.html', url_list=url_list)
-        
+
+        # Check if the URL is already shortened and not expired
         if original_url in url_list:
             existing_expiration_time = url_list[original_url].get('expiration_time')
             if existing_expiration_time:
@@ -45,14 +53,13 @@ def index():
             flash('This URL has already been shortened.')
             return render_template('index.html', url_list=url_list)
 
-        if original_url in url_list:
-            short_url = url_list[original_url]['short_url']
-        else:
+        # Generate short URL and store it
+        if original_url not in url_list:
             short_url = generate_short_url()
             url_list[original_url] = {'short_url': short_url, 'expiration_time': expiration_time, 'click_count': 0}
 
+        # Check expiration time for all URLs in the list
         current_time = datetime.now()
-
         for original, data in url_list.items():
             if data['expiration_time']:
                 dt_object = datetime.strptime(data['expiration_time'], '%Y-%m-%dT%H:%M')
@@ -61,8 +68,10 @@ def index():
         
         return redirect(url_for('index'))
 
+    # Render the index page for GET requests
     return render_template('index.html', url_list=url_list)
 
+# Route to handle URL redirection based on the short URL
 @app.route('/<short_url>')
 def redirect_to_original(short_url):
     for original, data in url_list.items():
@@ -75,11 +84,13 @@ def redirect_to_original(short_url):
     flash('URL not found!')
     return redirect(url_for('index'))
 
+# Route to handle URL deletion via POST request
 @app.route('/delete_url', methods=['POST'])
 def delete_url():
     data = request.json
     short_url_to_delete = data.get('url')
     
+    # Find and delete the URL from the list
     for original, data in list(url_list.items()): 
         if data['short_url'] == short_url_to_delete:
             del url_list[original]
@@ -87,7 +98,6 @@ def delete_url():
 
     return jsonify({'success': False, 'message': 'URL not found'}), 404
 
-
-
+# Run the Flask application
 if __name__ == '__main__':
     app.run(debug=True)
